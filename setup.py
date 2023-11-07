684325a00c6c04a7950a124851ba59d858c7e27a
Victor <victorhugopvh16@email.com>
2023-11-07 00:03:24 -0300

    Adicionado setup para criação de executável e icon

---
+++ setup.py
@@ -0,0 +1,31 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# script Python principal
+script = "Calculador_de_Hash.py"
+
+# Crie um executável
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"  #"Win32GUI" para aplicação GUI no Windows
+
+exe = Executable(
+    script=script,
+    base=base,
+    icon="C:\\Users\\Victor\\Downloads\\teste\\Icon\\wp.ico", #Hashtag 2 icon by Icons8
+)
+
+# Configurações para a criação do executável
+build_exe_options = {
+    "packages": ["hashlib", "tkinter", "pyperclip"],  # Lista de pacotes usados
+    "excludes": [],
+    "include_files": [],
+}
+
+setup(
+    name="CalculadoraDeHash",
+    version="1.0",
+    description="Simples calculadora de hash que utiliza o método sha256",
+    options={"build_exe": build_exe_options},
+    executables=[exe]
+)
